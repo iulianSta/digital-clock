38f06177ce78b2df2fa5f61ecd21cec0fb97053b
iulianstan <iulian1226@gmail.com>
2021-05-10 11:28:51 +0200

    src/scripts/index.js timeDisplay function

--- DigitalClockApp/src/scripts/index.js
+++ DigitalClockApp/src/scripts/index.js
@@ -1,3 +1,24 @@
 // Any code you will write for your website (in the future!) should go here
-// Select element
-let time = document.querySelector("#MyClockDisplay");
+// timeDisplay function
+const timeDisplay = () => {
+  // Select element
+  let clock = document.querySelector("#MyClockDisplay");
+  // Variables
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let session = "AM";
+  if (h == 0) h = 12;
+  else if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+  h = h < 10 ? `0${h}` : h;
+  m = m < 10 ? `0${m}` : m;
+  s = s < 10 ? `0${s}` : s;
+  let time = `${h}:${m}:${s} ${session}`;
+  clock.innerHTML += time;
+};
+
+console.log(timeDisplay());
